9c7792855dae660b90fb90d6156c2e71928e8287
Igor Bodnar <ln-e@users.noreply.github.com>
2018-10-11 11:09:46 +0300

    Update to rxjs6

--- src/jwt.interceptor.ts
+++ src/jwt.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { JwtHelperService } from './jwthelper.service';
 import { JWT_OPTIONS } from './jwtoptions.token';
 
